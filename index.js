054960e87d2bb2aa318601fb5f4013bdda6c5275
seasons986 <annalin12345@gmail.com>
2023-11-27 19:42:19 -0500

    tarott

--- index.js
+++ index.js
@@ -5,42 +5,37 @@ window.addEventListener("load", () => {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
 
-  const container = document.getElementById("container");
   const button = document.getElementById("draw");
-  const input = document.getElementById("input");
-
-  let tennis = "";
-
-  function createElement(initial, left=0, top=0, size=0) {
-    if (+(left) >= 400) return atBounds = true;
-    const pre = document.createElement("pre");
-    pre.innerHTML = "🎾";
-    let nleft = initial ? getRandomInt(0,100) : +(left)+5;
-    let ntop = initial ? getRandomInt(0,100): +(top)+5;
-    let nsize = initial ? 25 : size;
-    pre.style.left = nleft+"px";
-    pre.style.top = ntop+"px";
-    pre.style.fontSize = nsize+"px";
-    if (tennis.length > 0) tennis+="**"; // separator
-    const cactus = `${nleft},${ntop},${nsize}`;
-    tennis+=cactus;
-    container.appendChild(pre);
-  }
-
-  //if (container.dataset.tennis) //TODO
+  const input = document.getElementById("prompt");
+  
   let channelID = "reading-rrgifbt1te4";
   input.onchange = (e) => channelID = e.target.value;
 
+  let cardIndices = [];
+  let tarotCard = {};
+
+  function drawCard(max) {
+    let i = getRandomInt(0,max);
+    console.log({max,i});
+    if (cardIndices.includes(i)) return drawCard(max);
+    cardIndices.push(i);
+    return i;
+  }
+
   function drawCards() {
     fetch(`https://api.are.na/v2/channels/${channelID}/contents`).then((r) => r.json()).then((d) => {
-      console.log({channelID,d});
       for (let i = 0; i < 3; i++) {
-        let tarotName = "TODO";
-        e.preventDefault();
-        const text = `<!DOCTYPE html><html> <head> <title>${tarotName}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/tennis/index.css"/><script src=https://iguannalin.github.io/tennis/index.js></script></head> <body><div id="overlay"></div> <div id="container" data-tennis=${btoa(tennis)}></div></body></html>`;
+        // description
+        // title
+        // image.display.url
+        const data = d.contents;
+        const index = drawCard(data.length);
+        tarotCard = `${data[index].title}**${data[index].description}**${btoa(data[index].image.display.url)}`;
+        console.log({cardIndices, tarotCard});
+        const text = `<!DOCTYPE html><html> <head> <title>${tarotCard.name}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/tarot/index.css"/><script src=https://iguannalin.github.io/tarot/tarot.js></script></head> <body><div id="overlay"></div> <div id="container" data-tarot=${tarotCard}></div></body></html>`;
         const blob = new Blob([text], {type: "text/html"});
         const blobUrl = URL.createObjectURL(blob);
-        window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=400,height=400`);
+        window.open(blobUrl, '_blank', `popup,location,status,scrollbars,resizable,width=200,height=300,top=500,left=${(i*300)+100}`);
         window.URL.revokeObjectURL(blobUrl);
       }
     });
---
+++ tarot.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", () => {
+  const container = document.getElementById("container");
+  if (container.dataset.tarot) createCard(container.dataset.tarot);
+
+  function createCard(cd) {
+    const card = cd.split("**");
+    const h1 = document.createElement("h1");
+    h1.innerText = card[0];
+    const image = document.createElement("img");
+    image.src = atob(card[1]);
+    const p = document.createElement("p");
+    p.innerText = card[2];
+    container.appendChild(h1);
+    container.appendChild(image);
+    container.appendChild(p);
+  }
+
+});
