044fd9d160f392eefb61bc4d8163f756aba9d8bf
seasons986 <annalin12345@gmail.com>
2023-11-27 19:47:47 -0500

    cleanup

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ window.addEventListener("load", () => {
 
   function drawCard(max) {
     let i = getRandomInt(0,max);
-    console.log({max,i});
     if (cardIndices.includes(i)) return drawCard(max);
     cardIndices.push(i);
     return i;
@@ -25,13 +24,9 @@ window.addEventListener("load", () => {
   function drawCards() {
     fetch(`https://api.are.na/v2/channels/${channelID}/contents`).then((r) => r.json()).then((d) => {
       for (let i = 0; i < 3; i++) {
-        // description
-        // title
-        // image.display.url
         const data = d.contents;
         const index = drawCard(data.length);
         tarotCard = `${data[index].title}**${data[index].description}**${btoa(data[index].image.display.url)}`;
-        console.log({cardIndices, tarotCard});
         const text = `<!DOCTYPE html><html> <head> <title>${tarotCard.name}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/tarot/index.css"/><script src=https://iguannalin.github.io/tarot/tarot.js></script></head> <body><div id="container" data-tarot=${tarotCard}></div></body></html>`;
         const blob = new Blob([text], {type: "text/html"});
         const blobUrl = URL.createObjectURL(blob);
