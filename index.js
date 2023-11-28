40aa8df6b0ac962692112f03410d11e52fd29253
seasons986 <annalin12345@gmail.com>
2023-11-27 20:33:00 -0500

    clean css

--- index.js
+++ index.js
@@ -8,25 +8,26 @@ window.addEventListener("load", () => {
   const button = document.getElementById("draw");
   const input = document.getElementById("prompt");
   
-  let channelID = "thesaurus-unqdpo2f-q8";
+  let channelID = "nj2z2y9jw5q";
   input.onchange = (e) => channelID = e.target.value;
 
   let cardIndices = [];
   let tarotCard = {};
 
-  function drawCard(max) {
+  function drawCard(max, cards) {
     let i = getRandomInt(0,max);
-    if (cardIndices.includes(i)) return drawCard(max);
+    if (cardIndices.includes(i)) return drawCard(max, cards);
+    else if (!cards[i].image.display) return drawCard(max, cards);
     cardIndices.push(i);
-    return i;
+    return cards[i];
   }
 
   function drawCards() {
     fetch(`https://api.are.na/v2/channels/${channelID}/contents`).then((r) => r.json()).then((d) => {
       for (let i = 0; i < 3; i++) {
-        const data = d.contents;
-        const index = drawCard(data.length);
-        tarotCard = `${data[index].title.replaceAll(`"`,`'`)}**${data[index].description.replaceAll(`"`,`'`)}**${data[index].image.display.url}`;
+        const card = drawCard(d.contents.length, d.contents);
+        console.log(card);
+        tarotCard = `${card.title.replaceAll(`"`,`'`)}**${card.description.replaceAll(`"`,`'`)}**${card.image.display.url}`;
         const text = `<!DOCTYPE html><html> <head> <title>${tarotCard.name}</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <link rel="stylesheet" href="https://iguannalin.github.io/tarot/index.css"/><script src=https://iguannalin.github.io/tarot/tarot.js></script></head> <body><div id="container" data-tarot=${tarotCard}></div></body></html>`;
         const blob = new Blob([text], {type: "text/html"});
         const blobUrl = URL.createObjectURL(blob);
